26b8c54de8d7da51dd9b86bdecaf29926038078c
isaiahcurry8 <isaiahcurry8@gmail.com>
2018-10-24 19:47:29 -0700

    Update ng7-pre/src/app/app-routing.module.ts

--- ng7-pre/src/app/app-routing.module.ts
+++ ng7-pre/src/app/app-routing.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';     // Add this
-import { AboutComponent } from './about/about.component';  // Add this
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-     path: 'about/:id',    
-    component: AboutComponent
-  }
+  
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
